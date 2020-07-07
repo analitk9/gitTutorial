6b4ec767e6cfcacb25afc0b03ef99e55792ec63d
analitik9 <analitik9@gmail.com>
2020-07-07 11:06:47 +0300

    Initial Commit

---
+++ gitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitTutorial
+//
+//  Created by Denis Evdokimov on 07.07.2020.
+//  Copyright © 2020 Denis Evdokimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
